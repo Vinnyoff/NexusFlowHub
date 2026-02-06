29d9c7dc158205939bc388579eecd04e2d37da68
Jairo Bragança <jairobraganca2020@gmail.com>
2026-02-06 18:44:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4941779058-cbb90",
+  "appId": "1:728332571373:web:5457eb2b37f73c9144f2e4",
+  "apiKey": "AIzaSyBOaFMiQ4hs78GpAc_NrBoixcDmOUhPfys",
+  "authDomain": "studio-4941779058-cbb90.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "728332571373"
+};
